863ee070c3cdf8d029663b54435e8c5245535c29
Test <shebalnikov@mail.ru>
2019-10-23 20:41:15 +0300

    7.1

---
+++ Lab 7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7
+{
+
+    enum AccountType
+    {
+        Debet,
+        Credit
+
+    }
+
+
+    class BankAccount
+    {
+        private int account;
+        private int balance;
+        private AccountType type;
+
+        public int Account { get => account; set => account = value; }
+        public int Balance { get => balance; set => balance = value; }
+        public AccountType Type { get => type; set => type = value; }
+
+    }
+
+
+
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            BankAccount bankAccount = new BankAccount();
+            bankAccount.Account = 1;
+            bankAccount.Balance = 0;
+            bankAccount.Type = AccountType.Debet;
+
+            Console.ReadKey(); 
+
+        }
+    }
+}
